950c7341d18fcc7a5c2df2fd6b836b8a40dd0c1d
vincet0t15 <vincet0t15@gmail.com>
2025-08-05 10:38:47 +0800

    'project'

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin',
+                'is_active' => true,
+                'password' => bcrypt('admin123'),
+            ],
+
+        ];
+
+        User::insert($users);
+    }
+}
