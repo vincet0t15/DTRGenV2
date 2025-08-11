a5449076b35006e881c2282411839465a105231e
vincet0t15 <vincet0t15@gmail.com>
2025-08-11 08:45:11 +0800

    push

--- resources/js/components/paginationData.tsx
+++ resources/js/components/paginationData.tsx
@@ -12,6 +12,8 @@ export default function Pagination({ data }: { data: PaginatedDataResponse<any> 
                     <Link
                         key={index}
                         href={link.url ?? '#'}
+                        preserveState
+                        preserveScroll
                         className={`rounded border px-3 py-1 text-sm transition-colors ${
                             link.active
                                 ? 'bg-gray-900 text-white dark:bg-gray-100 dark:text-black'
